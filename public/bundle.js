1dfc7494fc08058f4ff3ede8133e453525ac609d
Vipin Sharma <vipinsharmaec@hotmail.com>
2018-04-19 00:26:01 +0530

    layout changes

--- public/bundle.js
+++ public/bundle.js
@@ -5151,7 +5151,7 @@ $(document).ready(function(){
     $( "#sortable" ).sortable({
         revert: true
       });
-    $( ".main-container" ).disableSelection();
+    $( "#main-container" ).disableSelection();
     $('.container').draggable({
         connectToSortable: "#sortable",
         helper: "clone",
@@ -5162,34 +5162,34 @@ $(document).ready(function(){
         animate: true
     });
 
-    // $(".full-width").draggable({
-    //     containment:"parent"
-    // })
-    $(".main-container").droppable({
+    $(".full-width").draggable({
+        containment:"parent",
+        helper: "clone",
+    })
+    $("#main-container").droppable({
         accept:".container",
         drop:function(event, ui){
-            $(".main-container-hover").removeClass("highlight");
+            $("#main-container-hover").removeClass("highlight");
       
-            $(".main-container").removeClass("dotted-border");
+            $("#main-container").removeClass("dotted-border");
             
-            $(".main-container").append(ui.draggable);
-            console.log(ui.draggable)
-            $(".main-container > .container").addClass("full-width");
-            $(".main-container > .container").attr("data-value", "drop-container")
+            $("#main-container").append(ui.draggable);
+            $("#main-container > .container").addClass("full-width");
+            $("#main-container > .container").attr("data-value", "drop-container")
         },
         over:function(event, ui){
-            $(".main-container-hover").addClass("highlight");
-            $(".main-container").addClass("dotted-border");
+            $("#main-container-hover").addClass("highlight");
+            $("#main-container").addClass("dotted-border");
         },
         out:function(event, ui){
-            $(".main-container-hover").removeClass("highlight");
-            $(".main-container").removeClass("dotted-border");
+            $("#main-container-hover").removeClass("highlight");
+            $("#main-container").removeClass("dotted-border");
         }
         // activate:function(event, ui){
-        //     $(".main-container-hover").addClass("highlight");
+        //     $("#main-container-hover").addClass("highlight");
         // },
         // deactivate:function(event, ui){
-        //     $(".main-container-hover").removeClass("highlight");
+        //     $("#main-container-hover").removeClass("highlight");
         // }
         // hoverClass:"highlight"
     });
